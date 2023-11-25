c67fa271559733a167aeb9f754510c94160d68f3
DariusEldritch <79668814+DariusEldritch@users.noreply.github.com>
2023-11-25 16:48:32 -0700

    Update relictPorthack.js

--- relictPorthack.js
+++ relictPorthack.js
@@ -2,14 +2,15 @@
 import {relictTPrint,relictFTPCrack,relictBruteSSH,relictNuke} from "relictLib.js";
 export async function main(ns) {
   var target = ns.args[0];
+  var skill = ns.args[1];
   var portReq = ns.getServerNumPortsRequired(target);
-  if (portReq > 2) {
+  if (portReq > skill) {
     relictTPrint(ns, "fuck");
   } if (portReq > 1) {
     relictFTPCrack(ns, target);
   } if (portReq > 0) {
     relictBruteSSH(ns, target);
-  } if ((portReq-2) >= portReq) {
+  } if ((portReq-skill) >= portReq) {
     relictNuke(ns, target);
   }
 }
